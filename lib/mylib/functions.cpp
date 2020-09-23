af8eb7f58b0b60b6101c32a963a38f8d4051baf0
Gabriel Soudry <gabriel_s@hotmail.fr>
2020-09-24 00:13:55 +0200

    c++ / 20

--- lib/mylib/functions.cpp
+++ lib/mylib/functions.cpp
@@ -1,19 +1,19 @@
 // #include <header.h>
+#include <header.h>
 #include <Arduino.h>
-#ifndef HEADER_H_
-    #include "header.h"
-#endif
 
-void encoderinterrupt(){
-    tick++;//increase tick count
+void encoderinterrupt()
+{
+    tick++; //increase tick count
 }
 
-void initializetimer(){
+void initializetimer()
+{
     // TIMER 1 for interrupt frequency 10 Hz:
-    cli(); // stop interrupts
+    cli();      // stop interrupts
     TCCR1A = 0; // set entire TCCR1A register to 0
     TCCR1B = 0; // same for TCCR1B
-    TCNT1  = 0; // initialize counter value to 0
+    TCNT1 = 0;  // initialize counter value to 0
     // set compare match register for 10 Hz increments
     OCR1A = 24999; // = 16000000 / (64 * 10) - 1 (must be <65536)
     // turn on CTC mode
@@ -25,21 +25,25 @@ void initializetimer(){
     sei(); // allow interrupts
 }
 
-void timerinterrupt(){
-    if(tduration>0){
+void timerinterrupt()
+{
+    if (tduration > 0)
+    {
         tduration--;
         // adjust error
-        e = (tspeed)-(tick);//should be devided by 0.1s for time and mult by R for distance
+        e = (tspeed) - (tick); //should be devided by 0.1s for time and mult by R for distance
         de = e - olde;
         E = E + e;
         //calculate pid values
-        val = kp*e+ki*E+kd*de;//calculate pid
-        mapped = map(val,0,maxpidout,0,255);//mapping from pidvals to pwm vals
-        analogWrite(pm1,mapped);//write the pid to the pwm
+        val = kp * e + ki * E + kd * de;         //calculate pid
+        mapped = map(val, 0, maxpidout, 0, 255); //mapping from pidvals to pwm vals
+        analogWrite(pm1, mapped);                //write the pid to the pwm
         //setup for next cycle
         tick = 0;
-        olde =e;
-    } else{
-        TIMSK1 &= ~(1 << OCIE1A);//disable timer compare interupt
+        olde = e;
+    }
+    else
+    {
+        TIMSK1 &= ~(1 << OCIE1A); //disable timer compare interupt
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
+#include <../lib/mylib/header.h>
 #include <Arduino.h>
-#ifndef HEADER_H_
-    #include "header.h"
-#endif
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(baudrate);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if(Serial.available() > 0)  {
-    int incomingData= Serial.parseInt();
-    tspeed = incomingData;//set the target speed
-    tduration = 20;//set the target duration
-    E=0;
-    olde=0;//this is a useless comment wow
+  if (Serial.available() > 0)
+  {
+    int incomingData = Serial.parseInt();
+    tspeed = incomingData; //set the target speed
+    tduration = 20;        //set the target duration
+    E = 0;
+    olde = 0; //this is a useless comment wow
   }
 }
