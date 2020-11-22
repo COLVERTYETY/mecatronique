0993deb97834ccee2693327bd03d626e08d24337
COLVERTYETY <niconoob1@gmail.com>
2020-11-22 20:34:21 +0100

    changed to the one created with the team

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,84 @@
-#include <Arduino.h>
-#include "header.h"
+#include <Arduino.h> 
 
-void encoderinterrupt()
-{
-    tick++; //increase tick count
-}
-void initializetimer()
-{
-    // TIMER 1 for interrupt frequency 50 Hz:
-    cli();      // stop interrupts
-    TCCR1A = 0; // set entire TCCR1A register to 0
-    TCCR1B = 0; // same for TCCR1B
-    TCNT1 = 0;  // initialize counter value to 0
-    // set compare match register for 50 Hz increments
-    OCR1A = 24999; // = 16000000 / (64 * 50) - 1 (must be <65536)
-    // turn on CTC mode
-    TCCR1B |= (1 << WGM12);
-    // Set CS12, CS11 and CS10 bits for 64 prescaler
-    TCCR1B |= (0 << CS12) | (1 << CS11) | (1 << CS10);
-    // enable timer compare interrupt
-    TIMSK1 |= (1 << OCIE1A);
-    sei(); // allow interrupts
-}
-void setup()
-{
-  // put your setup code here, to run once:
-  Serial.begin(baudrate);
-  pinMode(LED_BUILTIN,OUTPUT);
-  pinMode(pin_dira,OUTPUT);
-  pinMode(pin_dirb,OUTPUT);
-  pinMode(pin_pwm,OUTPUT);
-  pinMode(pin_encoder,INPUT);
-  initializetimer();
-  attachInterrupt(digitalPinToInterrupt(pin_encoder),encoderinterrupt,RISING);
-}
-ISR(TIMER1_COMPA_vect){
-   cli();
-    if(tduration > 0)
-    {
-        tduration--;
-        // adjust error
-        e = (tspeed) - (tick); //should be devided by 0.1s for time and mult by R for distance
-        E = E + e;
-        Ival = ki*E;
-        Ival = (Ival < maxI) ? Ival : maxI; //Ival saturation to prevent windup, not really windup though
-        //calculate pid values
-        val = kp * e + Ival;        //calculate pid
-        mapped = (val < 255) ? val : 255;
-        mapped = (val < 0) ? 0 : val;
-        analogWrite(pin_pwm, mapped);                //write the pid to the pwm
-        //setup for next cycle
-    }
-    else
-    {
-        //TIMSK1 &= ~(1 << OCIE1A); //disable timer compare interupt
-    }
-    oldtick = tick;
-    tick = 0;
-    mainloop=true;
-    sei();
-}
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  if(mainloop){
-    Serial.println(oldtick);
-    mainloop = false;
-    led_status = !led_status;
-    digitalWrite(LED_BUILTIN,led_status);
-  }
-}
+#define kp 0.3 // poids proportionnel 
+#define ki 0.3 // poinds intégral 
+#define kd 0.0 // poids dérivatif 
+#define targerRPM 120 
 
+volatile int e; // erreur proportionnelle 
+volatile int de; // delta erreur (erreur dérivative) 
+volatile int E = 0; // erreur intégrale 
+volatile int nbTic = 0; // nb de tics par cycle 
+volatile int olde = 0;  
 
+#define pin_enable 11 // a besoin du PWM 
+#define pin_dir1 12 // direction 1 
+#define pin_dir2 10 // direction 2 
+#define pin_encodeur 2 // a besoin du pin interrupt 
+#define N 11.0 // nb de tic par rotation du moteur 
+#define tps 10 // délai défini à 10ms 
+#define rapportDeReduction 0.05 // rapport de réduction entre la vitesse calculée du moteur et sa vitesse réelle 
+
+const float tpsEnMinute = tps/60000.00;  
+float R; // nombre de rotations par minute du moteur 
+bool dir = true; //permet de changer la direction 
+float V; // résultat de sortie du PID 
+bool led_status = true; //permet de controler la led integrer
+
+void encodeur_callback() 
+{ 
+  nbTic ++;  
+} 
+void setup()  
+{ 
+  // put your setup code here, to run once: 
+  pinMode(LED_BUILTIN, OUTPUT);  
+  pinMode (pin_enable, OUTPUT);  
+  pinMode (pin_dir1, OUTPUT);  
+  pinMode (pin_enable, OUTPUT);  
+  pinMode (pin_encodeur, INPUT);  
+  attachInterrupt (digitalPinToInterrupt(pin_encodeur), encodeur_callback, RISING); //A chaque tic on appelle encodeur_interrupt qui se chargera d'incrémenter de 1 nbTic 
+  digitalWrite(pin_enable, LOW); 
+  digitalWrite(pin_dir1, dir);  
+  digitalWrite(pin_dir2, !dir);   
+  Serial.begin(450000);  
+} 
+
+void loop()  
+{ 
+  // put your main code here, to run repeatedly: 
+  digitalWrite(LED_BUILTIN, led_status); 
+  led_status = !led_status;  
+  delay(tps); 
+  cli(); // interdire les interruptions 
+  R = nbTic/N;// rotations 
+  R *= rapportDeReduction; // obtenir le réel nombre de rotations 
+  R /= tpsEnMinute; // rotations en RPM 
+  e = targerRPM - R; // erreur mesurée  
+  de = e - olde; // delta erreur mesuré 
+  E += e; // somme des erreurs mesurées 
+  if (E>512)
+  {
+    E = 512;
+  } 
+  if (E<-512)
+  { 
+    E = -512;  
+  } 
+  V = e*kp + de*kd + E*ki; // calcul du PID (de 0V à 5V) 
+  V *= 51; // PID (de 0 à 255 (valeurs du "monde" du PWM))
+  if (V>255) 
+  { 
+    V = 255;  
+  } 
+  if (V<0) 
+  { 
+    V = 0; 
+  } 
+  analogWrite (pin_enable, (int)V); // on écrit sur pin_enable la valeur de V 
+  nbTic = 0; // on remet la valeur nbTic à 0 
+  olde = e;  
+  Serial.print(R);// on print la valeur des RPM obtenus 
+  Serial.println(" : RPM");  
+  sei(); // accepter de nouveau les interruptions (du encodeur_callback notamment) 
+} 
