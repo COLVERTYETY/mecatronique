5b51f07e3d78eb820fc8be75b7891ad31d665b70
COLVERTYETY <niconoob1@gmail.com>
2020-11-24 09:53:35 +0100

    reset nbtic in protected space

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ volatile int olde = 0;
 #define pin_dir1 12 // direction 1 
 #define pin_dir2 10 // direction 2 
 #define pin_encodeur 2 // a besoin du pin interrupt 
-#define N 224.4 // nb de tic par rotation du moteur vec reducteur
+#define N 224.4 // nb de tic par rotation du moteur avec reducteur
 #define tps 10 // délai défini à 10ms 
 
 const float tpsEnMinute = 60000.00/tps;  
@@ -34,6 +34,7 @@ ISR(TIMER1_COMPA_vect){
   //interrupt commands for TIMER 1 here, runs asynchonrously
   cli(); // interdire les interruptions
   tickcopy = nbTic;//copy of tick so that we can turn back on the interrupts
+  nbTic = 0;// on remet la valeur nbTic à 0
   sei();//turn th einterrupt sback on
 
   vitesse_rpm = tickcopy*ratio; // rotations en RPM 
@@ -50,12 +51,9 @@ ISR(TIMER1_COMPA_vect){
   if (res_PID>255){res_PID = 255;} //saturation
   if (res_PID<0){res_PID = 0;} //saturation
 
-  analogWrite (pin_enable, (int)res_PID); // on écrit sur pin_enable la valeur de res_pid
-
-  nbTic = 0; // on remet la valeur nbTic à 0 
+  analogWrite (pin_enable, (int)res_PID); // on écrit sur pin_enable la valeur de res_pid 
   olde = e;  
   Serial.println(vitesse_rpm);// on print la valeur des RPM obtenus 
-  //sei(); // accepter de nouveau les interruptions (du encodeur_callback notamment)
 } 
 void setup()  
 { 
@@ -66,32 +64,32 @@ void setup()
   pinMode (pin_enable, OUTPUT);  
   pinMode (pin_encodeur, INPUT);  
   attachInterrupt (digitalPinToInterrupt(pin_encodeur), encodeur_callback, RISING); //A chaque tic on appelle encodeur_interrupt qui se chargera d'incrémenter de 1 nbTic 
-  digitalWrite(pin_enable, LOW); 
-  digitalWrite(pin_dir1, dir);  
-  digitalWrite(pin_dir2, !dir);  
+  digitalWrite(pin_enable, LOW); //coupe l alimentation du moteur avant que le sens soit déclaré
+  digitalWrite(pin_dir1, dir);  //on declare le sen de rotation
+  digitalWrite(pin_dir2, !dir); //on declare le sen de rotation
 
-  Serial.begin(450000);
+  Serial.begin(450000); // on init la comm par serial
 
-  // TIMER 1 for interrupt frequency 100 Hz:
-  cli(); // stop interrupts
+  // TIMER 1 pour 100 hz
+  cli(); // on eteind les interruptions
   TCCR1A = 0; // set entire TCCR1A register to 0
   TCCR1B = 0; // same for TCCR1B
   TCNT1  = 0; // initialize counter value to 0
-  // set compare match register for 100 Hz increments
+  // on initialise un "buffer" pour le timer pour avoir la fréquence désiré
   OCR1A = 19999; // = 16000000 / (8 * 100) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
-  // Set CS12, CS11 and CS10 bits for 8 prescaler
+  // Set CS12, CS11 and CS10 bits for 8 prescaler pour controler la vitesse du timer
   TCCR1B |= (0 << CS12) | (1 << CS11) | (0 << CS10);
-  // enable timer compare interrupt
+  // on allume le timer 1
   TIMSK1 |= (1 << OCIE1A);
-  sei(); // allow interrupts 
+  sei(); // on rallume les interruptions
 } 
 
 void loop()  
 { 
   // put your main code here, to run repeatedly: 
-  digitalWrite(LED_BUILTIN, led_status); 
+  digitalWrite(LED_BUILTIN, led_status);//on fait clignoter la led intégré de facon bloquant
   led_status = !led_status;  
-  delay(1000);
+  delay(1000);//ceci est bloquant
 } 
